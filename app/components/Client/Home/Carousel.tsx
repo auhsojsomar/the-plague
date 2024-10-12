3dfed0fafeacd1aaa0e48ba4548d0721967a5a16
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-13 00:00:41 +0800

    feat(ui): :sparkles: add homepage carousel

--- app/components/Client/Home/Carousel.tsx
+++ app/components/Client/Home/Carousel.tsx
@@ -1,59 +1,33 @@
-"use client";
+import { Carousel } from "flowbite-react";
+import React from "react";
 
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import Image from "next/image";
-
-const imgWidth = 200;
-const imgHeight = 200;
-
-const carouselImgSrc = [
-  "https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp",
-  "https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp",
-  "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
-  "https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp",
-  "https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp",
-  "https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp",
-  "https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp",
-];
-
-export default function Carousel() {
-  const [carouselActiveItem, setCarouselActiveItem] = useState(0);
-
-  const carouselRef = useRef<HTMLDivElement>(null);
-
-  const scrollItem = () => {
-    setCarouselActiveItem((prevState) => {
-      if (carouselImgSrc.length - 1 > prevState) {
-        return prevState + 1;
-      } else {
-        return 0;
-      }
-    });
-  };
-
-  const autoplay = useCallback(() => setInterval(scrollItem, 1000), []);
-
-  useEffect(() => {
-    const play = autoplay();
-    return () => clearInterval(play);
-  }, [autoplay]);
-
-  useEffect(() => {
-    carouselRef.current?.scroll({ left: imgWidth * carouselActiveItem });
-  }, [carouselActiveItem]);
-
+const HomeCarousel = () => {
   return (
-    <div ref={carouselRef} className="carousel carousel-center">
-      {carouselImgSrc.map((imgSrc) => (
-        <div key={imgSrc} className="carousel-item">
-          <Image
-            width={imgWidth}
-            height={imgHeight}
-            src={imgSrc}
-            alt={imgSrc}
-          />
-        </div>
-      ))}
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 rounded-none">
+      <Carousel>
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+          alt="..."
+        />
+      </Carousel>
     </div>
   );
-}
+};
+
+export default HomeCarousel;
