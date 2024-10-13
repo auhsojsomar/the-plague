1a6c446cdfbfd09edd1a94750940e888305bca7e
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-13 14:40:12 +0800

    fix(ui): :lipstick: fix carousel responsiveness and make it full screen on larger device

--- app/components/Client/Home/Carousel.tsx
+++ app/components/Client/Home/Carousel.tsx
@@ -1,31 +1,55 @@
+import Image from "next/image";
 import { Carousel } from "flowbite-react";
-import React from "react";
+import type { CustomFlowbiteTheme } from "flowbite-react";
+import { Flowbite } from "flowbite-react";
+import styles from "./Carousel.module.css";
+
+import banner from "@/image/main-banner.jpg";
+
+const customTheme: CustomFlowbiteTheme = {
+  carousel: {
+    scrollContainer: {
+      base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth",
+      snap: "snap-x",
+    },
+  },
+};
 
 const HomeCarousel = () => {
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 rounded-none">
-      <Carousel>
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-          alt="..."
-        />
-      </Carousel>
+    <div className="h-56 sm:h-80 md:h-96 lg:h-auto max-w-screen-2xl mx-auto">
+      <Flowbite theme={{ theme: customTheme }}>
+        <Carousel slide={false}>
+          <div className="relative">
+            <Image
+              className={`${styles.carouselImage} object-contain lg:object-cover mx-auto`}
+              src={banner}
+              alt="banner-1"
+            />
+          </div>
+          <div className="relative">
+            <img
+              className={`${styles.carouselImage} object-contain lg:object-cover mx-auto`}
+              src="https://placehold.co/1536x650?text=Place+your+image+here"
+              alt="banner-2"
+            />
+          </div>
+          <div className="relative">
+            <img
+              className={`${styles.carouselImage} object-contain lg:object-cover mx-auto`}
+              src="https://placehold.co/1536x650?text=Place+your+image+here"
+              alt="banner-3"
+            />
+          </div>
+          <div className="relative">
+            <img
+              className={`${styles.carouselImage} object-contain lg:object-cover mx-auto`}
+              src="https://placehold.co/1536x650?text=Place+your+image+here"
+              alt="banner-4"
+            />
+          </div>
+        </Carousel>
+      </Flowbite>
     </div>
   );
 };
