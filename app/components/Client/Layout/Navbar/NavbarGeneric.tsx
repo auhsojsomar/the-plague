d836f7d0b8b9a05ddfd10bff02003d6588bbcafd
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-15 09:54:56 +0800

    style(ui): :lipstick: make navbar fixed on top

--- app/components/Client/Layout/Navbar/NavbarGeneric.tsx
+++ app/components/Client/Layout/Navbar/NavbarGeneric.tsx
@@ -5,7 +5,7 @@ import { NavbarContextProvider } from "@/app/context/NavbarContext";
 
 const NavbarGeneric = () => {
   return (
-    <nav className="bg-black px-5 sm:px-0 sm:h-20 p-2 sm:py-0 relative opacity-90 z-10">
+    <nav className="bg-secondary-color px-5 sm:px-0 sm:h-20 p-2 sm:py-0 fixed w-full z-10">
       <div className="container flex flex-wrap items-center justify-between h-full">
         <Image
           src="/favicon.ico"
