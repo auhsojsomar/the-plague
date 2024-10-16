85549fc3363123c4efdc2162ce687ebf3c0c05a0
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-16 15:44:45 +0800

    fix: :bug: update z index of navbar to fix showing sale badge above the navbar

--- app/components/Client/Layout/Navbar/NavbarGeneric.tsx
+++ app/components/Client/Layout/Navbar/NavbarGeneric.tsx
@@ -5,7 +5,7 @@ import { NavbarContextProvider } from "@/app/context/NavbarContext";
 
 const NavbarGeneric = () => {
   return (
-    <nav className="bg-secondary-color px-5 sm:px-0 sm:h-20 p-2 sm:py-0 fixed w-full z-10">
+    <nav className="bg-secondary-color px-5 sm:px-0 sm:h-20 p-2 sm:py-0 fixed w-full z-50">
       <div className="container flex flex-wrap items-center justify-between h-full">
         <Image
           src="/image/logo.jpg"
