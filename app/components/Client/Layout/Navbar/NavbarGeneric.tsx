1c735bc75d830890b92a3a095fb45d37abb16131
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-16 14:09:01 +0800

    fix: :building_construction: change logo path from public folder and move the assets image folder to public folder

--- app/components/Client/Layout/Navbar/NavbarGeneric.tsx
+++ app/components/Client/Layout/Navbar/NavbarGeneric.tsx
@@ -8,8 +8,8 @@ const NavbarGeneric = () => {
     <nav className="bg-secondary-color px-5 sm:px-0 sm:h-20 p-2 sm:py-0 fixed w-full z-10">
       <div className="container flex flex-wrap items-center justify-between h-full">
         <Image
-          src="/favicon.ico"
-          alt="Picture of the author"
+          src="/image/logo.jpg"
+          alt="the-plague-icon"
           width={60}
           height={60}
         />
