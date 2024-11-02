c5db6b46ea7479a4d92da0d86b53d28bbd37b60f
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-03 07:42:51 +0800

    style: :lipstick: Remove loading opacity

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "flowbite-react";
 
 const Loading = () => {
   return (
-    <div className="absolute inset-0 bg-gray-700/70 flex justify-center items-center">
+    <div className="absolute inset-0 bg-gray-700 flex justify-center items-center">
       <Spinner className="w-16 h-16" aria-label="Loading..." />
     </div>
   );
