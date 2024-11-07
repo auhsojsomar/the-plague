be01184986a9e788739243ff5ed3796b06f296bf
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-07 19:38:14 +0800

    refactor(refactor): :coffin: Remove React from import

--- src/components/Client/Home/Home.tsx
+++ src/components/Client/Home/Home.tsx
@@ -1,6 +1,5 @@
-import React, { lazy } from "react";
+import { lazy } from "react";
 
-// Lazy load the components using React.lazy
 const Carousel = lazy(() => import("./Carousel/Carousel"));
 const ProductBanner = lazy(() => import("./ProductBanner/ProductBanner"));
 const BestProduct = lazy(() => import("./BestProduct/BestProduct"));
