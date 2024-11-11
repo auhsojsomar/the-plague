d645d5f50254c2e59724e47346613b69ab952290
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-12 01:34:21 +0800

    feat(api): :sparkles: Add upload image to s3 bucket

--- src/app/api/s3-bucket/route.ts
+++ src/app/api/s3-bucket/route.ts
@@ -1,8 +1,14 @@
 "use server";
 
 import { NextResponse } from "next/server";
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { v4 as uuidv4 } from "uuid";
+import sharp from "sharp";
 
 const s3Client = new S3Client({
   region: process.env.NEXT_PUBLIC_AWS_REGION,
@@ -11,6 +17,26 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
   },
 });
+
+const generateUniqueFileName = () => {
+  const timestamp = Date.now();
+  const uniqueId = uuidv4();
+  return `${timestamp}-${uniqueId}.webp`;
+};
+
+const uploadFileToS3 = async (
+  fileContent: Buffer,
+  fileName: string,
+  bucketName: string
+) => {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: fileName,
+    Body: fileContent,
+    ContentType: "image/webp",
+  });
+  await s3Client.send(command);
+};
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -47,5 +73,45 @@ export async function GET(req: Request) {
     const errorMessage =
       (error as Error).message || "Error generating presigned URL";
     return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const contentType = req.headers.get("content-type");
+
+  if (!contentType || !contentType.startsWith("multipart/form-data")) {
+    return NextResponse.json(
+      { error: "Invalid content type" },
+      { status: 400 }
+    );
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("file");
+
+  if (!file || !(file instanceof Blob)) {
+    return NextResponse.json(
+      { error: "No valid file uploaded" },
+      { status: 400 }
+    );
+  }
+
+  const fileBuffer = await file.arrayBuffer();
+  const webpBuffer = await sharp(Buffer.from(fileBuffer)).webp().toBuffer();
+
+  const fileName = `transaction/${generateUniqueFileName()}`;
+  const bucketName = process.env.AWS_BUCKET_NAME!;
+
+  try {
+    await uploadFileToS3(webpBuffer, fileName, bucketName);
+
+    const fileUrl = fileName;
+    return NextResponse.json({ fileUrl });
+  } catch (uploadError) {
+    console.error("File upload error:", uploadError);
+    return NextResponse.json(
+      { error: "Failed to upload file" },
+      { status: 500 }
+    );
   }
 }
