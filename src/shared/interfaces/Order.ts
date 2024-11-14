44870de7d2ce8d70fe9c486b3ba355e9bafc16b0
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-14 23:31:51 +0800

    ci: :card_file_box: Add Order interface for getting the data from api

---
+++ src/shared/interfaces/Order.ts
@@ -0,0 +1,59 @@
+interface User {
+  id: string;
+  email: string | null;
+  fullName: string | null;
+  shippingAddress: Address[] | null;
+}
+
+interface OrderStatus {
+  key: number;
+  name: string;
+  isActive: number;
+}
+
+interface PaymentMethod {
+  key: number;
+  name: string;
+  isActive: number;
+}
+
+interface PaymentStatus {
+  key: number;
+  name: string;
+  isActive: number;
+}
+
+interface ShippingFee {
+  key: number;
+  name: string;
+  isActive: number;
+}
+
+interface Address {
+  id: string;
+  default: number;
+  fullName: string;
+  address: string;
+  contactNumber: string;
+}
+
+interface OrderItem {
+  product: string;
+  variant: string;
+  quantity: number;
+}
+
+export interface Order {
+  id: string;
+  user: User;
+  orderStatus: OrderStatus;
+  paymentMethod: PaymentMethod;
+  paymentStatus: PaymentStatus;
+  shippingFee: ShippingFee;
+  paymentTransactionFile: string;
+  shippingAddress: Address;
+  totalPrice: number;
+  items: OrderItem[];
+  dateCreated: string;
+  dateModified: string;
+}
