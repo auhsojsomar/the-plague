9a2c4bd59a9e763f7c4b5ce8cb2cdff35ead2fa2
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-14 11:46:22 +0800

    fix: :bug: Use the adminId and token directly from login

--- src/lib/api/tokenApi.ts
+++ src/lib/api/tokenApi.ts
@@ -1,8 +1,3 @@
-import Cookies from "js-cookie";
-
-const authToken = Cookies.get("authToken");
-const adminId = Cookies.get("adminId");
-
 interface AdminData {
   adminId: string;
   username: string;
@@ -10,32 +5,28 @@ interface AdminData {
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const verifyAdminToken = async (): Promise<boolean> => {
-  if (!authToken) {
-    throw new Error("No auth token found.");
-  }
+export const verifyAdminToken = async (
+  token: string,
+  adminId: string | undefined
+): Promise<boolean> => {
+  if (!token) throw new Error("No auth token found.");
 
   try {
     const response = await fetch(`${BASE_URL}/Token/verify-token`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${authToken}`,
+        Authorization: `Bearer ${token}`,
       },
     });
 
     if (!response.ok) {
-      if (response.status >= 500) {
-        throw new Error("Server error. Please try again later.");
-      } else {
-        const errorData = await response.json();
-        throw new Error(errorData.message || "An unknown error occurred.");
-      }
+      const errorData = await response.json();
+      throw new Error(errorData.message || "An unknown error occurred.");
     }
 
-    const data: AdminData = await response.json();
+    const data = await response.json();
 
-    if (data.adminId === adminId) return true;
-    return false;
+    return data.adminId === adminId;
   } catch (error) {
     throw new Error(
       error instanceof Error ? error.message : "An unexpected error occurred."
