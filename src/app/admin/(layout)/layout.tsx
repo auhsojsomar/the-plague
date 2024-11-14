7fbe1b24815f0c88a7e0646e3c5e3a54a69b22bd
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-14 11:33:59 +0800

    feat: :truck: rename admin layout from (sidebar) to (layout)

---
+++ src/app/admin/(layout)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/src/globals.css";
+import AdminLayout from "@/src/components/Admin/Layout/Layout";
+
+export const metadata: Metadata = {
+  title: "The Plague Admin Dashboard",
+  description:
+    "Admin portal for managing The Plague clothing shop inventory, orders, and settings.",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" data-theme="light">
+      <body className={inter.className}>
+        <AdminLayout>{children}</AdminLayout>
+      </body>
+    </html>
+  );
+}
