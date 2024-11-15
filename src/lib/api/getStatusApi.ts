11ae59f6ca09bc7a48330814e827b7225b9e1f30
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-15 20:51:55 +0800

    feat(api): :alien: Add `getStatusApi` for getting the order status and payment status

---
+++ src/lib/api/getStatusApi.ts
@@ -0,0 +1,72 @@
+export interface StatusProps {
+  key: number;
+  name: string;
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const getOrderStatus = async (): Promise<StatusProps[]> => {
+  try {
+    const response = await fetch(`${BASE_URL}/OrderStatus`);
+
+    if (!response.ok) {
+      if (response.status >= 500) {
+        throw new Error("Server error. Please try again later.");
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "An unknown error occurred.");
+      }
+    }
+
+    const data: StatusProps[] = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "An unexpected error occurred."
+    );
+  }
+};
+
+export const getPaymentStatus = async (): Promise<StatusProps[]> => {
+  try {
+    const response = await fetch(`${BASE_URL}/PaymentStatus`);
+
+    if (!response.ok) {
+      if (response.status >= 500) {
+        throw new Error("Server error. Please try again later.");
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "An unknown error occurred.");
+      }
+    }
+
+    const data: StatusProps[] = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "An unexpected error occurred."
+    );
+  }
+};
+
+export const getPaymentMethod = async (): Promise<StatusProps[]> => {
+  try {
+    const response = await fetch(`${BASE_URL}/PaymentMethod`);
+
+    if (!response.ok) {
+      if (response.status >= 500) {
+        throw new Error("Server error. Please try again later.");
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "An unknown error occurred.");
+      }
+    }
+
+    const data: StatusProps[] = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "An unexpected error occurred."
+    );
+  }
+};
