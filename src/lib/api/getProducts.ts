cb34d1aa363bc96b2badebead1fcddfced11f261
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-22 16:31:58 +0800

    refactor: :recycle: Make getProducts api reusable

---
+++ src/lib/api/getProducts.ts
@@ -0,0 +1,20 @@
+import { Product } from "@/types/Product";
+import { BASE_URL } from "./BASE_URL";
+
+export const getProducts = async (): Promise<Product[]> => {
+  try {
+    const response = await fetch(`${BASE_URL}/products`, {
+      next: { revalidate: 30 }, // Re-fetch after 30 seconds
+    });
+
+    if (!response.ok) {
+      console.error("Failed to fetch products:", response.statusText);
+      return [];
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
+};
