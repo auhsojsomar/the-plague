1e8c8bba016c5422d534bd27a04878ccb43d368d
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-29 05:53:50 +0800

    feat: :necktie: Add `updateProduct` on api call in admin products

--- src/lib/api/adminProduct.ts
+++ src/lib/api/adminProduct.ts
@@ -3,21 +3,20 @@ import { InsertProductDto } from "@/src/shared/interfaces/InsertProductDto";
 
 export const insertProduct = async (
   product: InsertProductDto
-): Promise<string> => {
+): Promise<InsertProductDto> => {
   try {
     const response = await fetch(`${BASE_URL}/Products`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json", // Ensure the server knows we're sending JSON
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(product), // Send the product object as a JSON string
+      body: JSON.stringify(product),
     });
 
     if (!response.ok) {
       if (response.status >= 500) {
         throw new Error("Server error. Please try again later.");
       } else if (response.status === 409) {
-        // Handle 409 Conflict error specifically
         const errorData = await response.json();
         throw new Error(errorData.message || "Product already exists.");
       } else {
@@ -26,7 +25,37 @@ export const insertProduct = async (
       }
     }
 
-    return "Product inserted successfully!";
+    return response.json();
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "An unexpected error occurred."
+    );
+  }
+};
+
+export const updateProduct = async (
+  product: InsertProductDto,
+  id?: string
+): Promise<string> => {
+  try {
+    const response = await fetch(`${BASE_URL}/Products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    if (!response.ok) {
+      if (response.status >= 500) {
+        throw new Error("Server error. Please try again later.");
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "An unknown error occurred.");
+      }
+    }
+
+    return "Product updated successfully!";
   } catch (error) {
     throw new Error(
       error instanceof Error ? error.message : "An unexpected error occurred."
