34ed65925bd4d1262901d174a7167178ddfb5d43
Joshua Ramos <auhsojsomar@gmail.com>
2024-12-03 23:23:50 +0800

    refactor: :recycle: Make process products reusable for product card

--- src/app/(client)/products/page.tsx
+++ src/app/(client)/products/page.tsx
@@ -1,27 +1,6 @@
 import Product from "@/src/components/Client/Product/Product";
 import { getProducts } from "@/src/lib/api/getProductsApi";
-import { Variant } from "@/src/shared/interfaces/Variant";
-import { Product as ProductType } from "@/src/shared/types/Product";
-
-const getBestVariant = (variants: Variant[]) => {
-  return variants.reduce((best, current) => {
-    const bestPrice = best.salePrice ?? best.price;
-    const currentPrice = current.salePrice ?? current.price;
-    return currentPrice < bestPrice ? current : best;
-  });
-};
-
-const processProducts = (products: ProductType[]) => {
-  return products.map((product) => {
-    const selectedVariant = getBestVariant(product.variants);
-    return {
-      ...product,
-      price: selectedVariant.price,
-      salePrice: selectedVariant.salePrice,
-      isSale: Boolean(selectedVariant.salePrice),
-    };
-  });
-};
+import { processProducts } from "@/src/utils/productUtils";
 
 const ProductListPage = async () => {
   const products = await getProducts();
